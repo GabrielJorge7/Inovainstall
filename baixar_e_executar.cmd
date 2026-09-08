117881cbcc0e06110d1761287ab1cbf179333058
Gabriel Jorge Marta <gabriel.jorge@inovafarma.com.br>
2026-09-08 16:14:43 -0300

    Ajusta atualizador e remove UPX

--- baixar_e_executar.cmd
+++ baixar_e_executar.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0baixar_e_executar.ps1"
+powershell.exe -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "%~dp0baixar_e_executar.ps1"
 if errorlevel 1 (
     echo.
     echo Ocorreu um erro. A janela permanecera aberta para leitura.
